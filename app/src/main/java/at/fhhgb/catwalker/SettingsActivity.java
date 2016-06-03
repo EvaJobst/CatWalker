cc7055c22c9ffd23bf8b640d492201edcfd56044
EvaJobst <S1510237014@students.fh-hagenberg.at>
2016-06-03 23:23:57 +0200

    - Changed Floating Action Button icon - Created Branch "settings" - Begin to work on Settings

--- app/src/main/java/at/fhhgb/catwalker/SettingsActivity.java
+++ app/src/main/java/at/fhhgb/catwalker/SettingsActivity.java
@@ -45,7 +45,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
 
     /**
      * A preference value change listener that updates the preference's summary
-     * to reflect its new value.
+     * to reflect its new value
      * Necessary for ringtone changes
      */
     private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
