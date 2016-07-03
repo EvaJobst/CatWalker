623a3fa94f65e2320fc5dc33e52f8143612362cb
EvaJobst <S1510237014@students.fh-hagenberg.at>
2016-07-03 13:08:31 +0200

    - new floating action button icon

--- app/src/main/java/at/fhhgb/catwalker/activity/TimelineActivity.java
+++ app/src/main/java/at/fhhgb/catwalker/activity/TimelineActivity.java
@@ -17,6 +17,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import at.fhhgb.catwalker.R;
