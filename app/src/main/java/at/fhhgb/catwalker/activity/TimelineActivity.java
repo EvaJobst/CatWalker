7845d130adb51dccc97fe9818a20134bb06a57ca
EvaJobst <S1510237014@students.fh-hagenberg.at>
2016-07-03 16:19:16 +0200

    - new colour scheme

--- app/src/main/java/at/fhhgb/catwalker/activity/TimelineActivity.java
+++ app/src/main/java/at/fhhgb/catwalker/activity/TimelineActivity.java
@@ -13,11 +13,9 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ImageButton;
 import android.widget.Toast;
 
 import at.fhhgb.catwalker.R;
@@ -33,7 +31,6 @@ public class TimelineActivity extends AppCompatActivity
     ActionBarDrawerToggle toggle;
     DrawerLayout drawer;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,8 +44,12 @@ public class TimelineActivity extends AppCompatActivity
         drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.str_nav_drawer_open, R.string.str_nav_drawer_close);
-        drawer.addDrawerListener(toggle);
+
         toggle.syncState();
+        drawer.addDrawerListener(toggle);
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
@@ -106,13 +107,19 @@ public class TimelineActivity extends AppCompatActivity
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        Log.i("Toggle", String.valueOf(id));
+        if(item.getItemId() == android.R.id.toggle) {
+            if(drawer.isDrawerOpen(GravityCompat.START)) {
+                drawer.closeDrawer(GravityCompat.START);
+            }
 
-        if(toggle.onOptionsItemSelected(item)) {
+            else {
+                drawer.openDrawer(GravityCompat.START);
+            }
+
             return true;
         }
 
+        Toast.makeText(this, "onOptionsItemSelected", Toast.LENGTH_SHORT).show();
         return super.onOptionsItemSelected(item);
     }
 
